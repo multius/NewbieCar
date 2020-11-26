b95e6bab495ba472e1790f4646035201d34c3e5d
Multi-Illusion <multiillusion@gmail.com>
2020-11-26 18:14:59 +0800

    kk

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ use nb::block;
 
 use cortex_m_rt::entry;
 use embedded_hal::digital::v2::OutputPin;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use stm32f1xx_hal::{pac, prelude::*, timer::Timer, i2c, i2c::BlockingI2c, stm32};
+
+mod registers;
 
 #[entry]
 fn main() -> ! {
@@ -21,6 +23,7 @@ fn main() -> ! {
     // HAL structs
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
@@ -32,9 +35,28 @@ fn main() -> ! {
     // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
     // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpiob.pb5.into_push_pull_output(&mut gpiob.crl);
+    let mut PB6 = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+    let mut PB7 = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
     // Configure the syst timer to trigger an update every second
     let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(1.hz());
 
+
+    
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (PB6, PB7),
+        &mut afio.mapr,
+        i2c::Mode::standard(100.hz()),
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        1,
+        1000,
+        1000
+    );
+
+    
     // Wait for the timer to trigger an update and change the state of the LED
     loop {
         block!(timer.wait()).unwrap();
